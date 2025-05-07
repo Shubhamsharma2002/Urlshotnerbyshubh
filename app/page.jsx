7efd2ae3f45c3251a2742bc31d428944ceb90e69
shubham sharma <shubhamtech004@gmail.com>
2025-05-07 19:24:56 +0530

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+          <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
+                 url shotner coming soon
+          </div>
+  );
+}
