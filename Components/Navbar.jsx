715677beef5dc31f0caf9c67450e10a48f46c0e1
shubham sharma <shubhamtech004@gmail.com>
2025-05-09 23:32:03 +0530

    navbar code done

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='p-3 bg-fuchsia-400 flex justify-around'>
+        <div>
+        <p>Logo</p>
+
+        </div>
+       <div className='flex gap-10 text-white font-semibold'> 
+        <p>About</p>
+        <p>Contact</p>
+        </div>
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
+import Navbar from "@/Components/Navbar";
 
 
 export default function Home() {
   return (
-          <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
+           <>
+           <Navbar/>
+           <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
                  url shotner coming soon
-          </div>
+           </div>
+           </>
+          
   );
 }
