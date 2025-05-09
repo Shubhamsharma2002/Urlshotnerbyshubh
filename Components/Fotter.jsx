e902e671e91dd17d78fba938cba5df60155c7548
shubham sharma <shubhamtech004@gmail.com>
2025-05-09 23:47:58 +0530

    footer section done

--- Components/Fotter.jsx
+++ Components/Fotter.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Fotter() {
   return (
-    <div>Fotter</div>
+    <div className='p-5 bg-black text-center text-white font-bold'>
+       All rigth reserved with Shubham Sharma
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,18 @@
+import Fotter from "@/Components/Fotter";
 import Navbar from "@/Components/Navbar";
-
 
 export default function Home() {
   return (
-           <>
-           <Navbar/>
-           <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
-                 url shotner coming soon
-           </div>
-           </>
-          
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      
+      <main className="flex-grow flex items-center justify-center">
+        <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
+          url shortner coming soon
+        </div>
+      </main>
+
+      <Fotter />
+    </div>
   );
 }
