20430266e4eb69d4a7d28163cb73b252a0440c16
shubham sharma <shubhamtech004@gmail.com>
2025-05-10 17:46:52 +0530

    project setup

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,6 @@ import Navbar from "@/Components/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      
-      <main className="flex-grow flex items-center justify-center">
-        <div className="text-blue-600 text-center font-bold mt-6 text-3xl">
-          url shortner coming soon
-        </div>
-      </main>
-
-      <Fotter />
-    </div>
+            <div>yes working on this</div>
   );
 }
