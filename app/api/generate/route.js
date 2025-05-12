9de29e5994c46884e91f7564a5bf5cd15c001ce5
shubham sharma <shubhamtech004@gmail.com>
2025-05-12 23:34:25 +0530

    api created and tested

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,8 +1,42 @@
 import connectDB from "@/libs/db";
+import ShortURL from "@/models/Shorturl";
+
 import { NextResponse } from "next/server";
 
-export const POST = async()=>{
-   
+export const POST = async (req) => {
+  await connectDB();
 
-   return NextResponse.json({ message: "This is the GET endpoint" });
-}
+  const body = await req.json();
+
+  const { url, shorturl } = body;
+
+  if (!url || !shorturl) {
+    return NextResponse.json({
+      success: false,
+      error: true,
+      message: "Missing required fields: url or shorturl",
+    }, { status: 400 });
+  }
+
+  const existing = await ShortURL.findOne({ shortCode: shorturl });
+
+  if (existing) {
+    return NextResponse.json({
+      success: false,
+      error: true,
+      message: "Short URL already exists!",
+    }, { status: 409 });
+  }
+
+  const result = await ShortURL.create({
+    originalURL: url,
+    shortCode: shorturl,
+  });
+
+  return NextResponse.json({
+    success: true,
+    error: false,
+    message: "URL generated successfully",
+    data: result,
+  });
+};
