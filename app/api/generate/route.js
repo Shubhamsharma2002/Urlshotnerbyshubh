6a6ffd32bbec2fd2a44020570683dbf0145ea245
shubham sharma <shubhamtech004@gmail.com>
2025-05-12 18:28:32 +0530

    db connection done

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,5 +1,7 @@
+import connectDB from "@/libs/db";
 import { NextResponse } from "next/server";
 
 export const POST = ()=>{
+   connectDB()
    return NextResponse.json({ message: "This is the GET endpoint" });
 }
